5161e2bade48802945e75159e21bf648e66c4295
whitesnakeit <tomasz.kapusniak.it@gmail.com>
2023-08-06 11:39:44 +0200

    Initial commit

---
+++ src/main/java/com/kapusniak/tomasz/typescripted/TypescriptedApplication.java
@@ -0,0 +1,13 @@
+package com.kapusniak.tomasz.typescripted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypescriptedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypescriptedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kapusniak/tomasz/typescripted/TypescriptedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kapusniak.tomasz.typescripted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypescriptedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
